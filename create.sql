b42fef22486847264f0573deb97f6a76c52e2f2a
Kareem eid <kareem.m.eid@gmail.com>
2021-12-09 22:28:14 +0200

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,142 @@
+﻿--create Database postgrad_office
+--create table Users(ID int primary Key ,first_name Varchar(50),last_name varchar(50), email varchar(50))
+--create table admins(ID int Primary key ,foreign key(ID) references Users on delete cascade )
+create table student(
+ID int primary key,
+first_name Varchar(20),
+last_name varchar(20),
+email varchar(50),
+faculty varchar(20),
+type Int,
+GPA decimal(3,2),
+address varchar(10),
+password varchar(20))
+
+create table student_mobile(
+sid int,
+mobile_number varchar(20),
+primary key(sid,mobile_number),
+foreign key(sid) references student on delete cascade)
+
+create table GUCian (
+ID int primary key ,
+undergrad_ID int,
+foreign key(ID) references Student)
+
+create table non_GUCian(
+ID int primary key,
+foreign key(ID) references student)
+
+create table course( 
+ID int primary key,
+credit_hours int,
+code varchar(10),
+fees int)
+
+create table thesis (
+serial_num varchar(50) primary key,
+field varchar(50),
+start_date datetime,
+end_date datetime,
+title varchar(50),
+type varchar(50),
+num_extensions int,
+years_spent as year(end_date) - year(start_date))
+
+create table payment (
+ID int primary key,
+num_installments int,
+total_amount int,
+fund_precentage int ,
+serial_num varchar(50),
+foreign key(serial_num) references thesis on delete cascade)
+
+create table installment(
+installment_num int identity,
+pid int,
+date datetime,
+status bit,
+amount int,
+primary key(installment_num,pid),
+foreign key(pid) references payment on delete cascade )
+
+create table supervisor(
+ID int primary key ,
+faculty varchar(20), 
+first_name Varchar(20),
+last_name varchar(20),
+email varchar(50),
+password varchar(20),
+address varchar(10))
+
+create table publication (
+title varchar(50) primary key ,
+host varchar(50),
+place varchar(50),
+date datetime ,
+is_accepted bit,
+sid int,
+pid int ,
+foreign key(sid) references student on delete cascade,
+foreign key(pid) references payment on delete cascade)
+
+create table defense(
+date datetime ,
+serial_num varchar(50),
+grade varchar(50),
+location varchar(50),
+sid int ,
+primary key(date ,serial_num),
+foreign key(serial_num) references thesis on delete cascade ,
+foreign key(sid) references student on delete cascade)
+
+create table examiner(
+ID int primary key,
+name varchar(50),
+field_of_work varchar(50),
+is_national bit)
+
+create table progress_report(
+report_num int,
+serial_num varchar(50),
+date datetime,
+description varchar(50),
+evaluation int,
+progress_state int ,
+sid int,
+vid int,
+primary key(report_num,serial_num),
+foreign key(serial_num) references thesis on delete no action,
+foreign key(sid) references student  on delete no action,
+foreign key(vid) references supervisor on delete no action)
+
+create table make_under_supervision(
+sid int,
+vid int,
+serial_num varchar(50) primary key,
+foreign key(sid) references student on delete no action,
+foreign key(vid) references supervisor on delete no action,
+foreign key(serial_num) references thesis on delete no action)
+
+create table take_course(
+sid int ,
+cid int ,
+grade varchar(50),
+primary key(sid,cid),
+foreign key(sid) references student on delete cascade,
+foreign key(cid) references course on delete cascade)
+
+create table publicaton_of_thesis(
+serial_num varchar(50),
+title varchar(50),
+primary key(serial_num, title),
+foreign key(serial_num) references thesis on delete no action,
+foreign key(title) references publication on delete no action)
+
+create table evaluates (
+eid int,
+date datetime,
+serial_num varchar(50),
+primary key(eid,date,serial_num),
+foreign key(eid) references examiner on delete cascade,
+foreign key(date,serial_num) references defense on delete cascade)
---
+++ proc.sql
@@ -0,0 +1,84 @@
+﻿create proc AdminViewSupervisorProfile
+@supId int
+as 
+select * from supervisor 
+where id = @supId
+
+go
+create proc AdminViewOnGoingTheses
+@thesesCount int output
+as
+select @thesesCount = count(*) from thesis
+where current_timestamp >= start_date and 
+(end_date is null or current_timestamp <= end_date)
+
+go
+create proc AdminListNonGucianCourse
+@courseID int
+as
+select s.first_name+' '+s.last_name as name, c.code, t.grade 
+from student s, take_course t, course c
+where s.ID = t.sid and c.ID = t.cid and t.cid = @courseID 
+
+go
+create proc AdminIssueThesisPayment
+@ThesisSerialNo int,
+@amount decimal,
+@noOfInstallments int,
+@fundPercentage decimal,
+@success bit output
+as
+if @ThesisSerialNo is null or @amount is null or @noOfInstallments is null or @fundPercentage is null
+set @success = 0
+else
+set @success = 1
+insert into payment (serial_num, total_amount, num_installments, fund_precentage) 
+values (@ThesisSerialNo, @amount, @noOfInstallments, @fundPercentage)
+
+go
+create proc AdminIssueInstallPayment
+@paymentID int, 
+@InstallStartDate date
+as
+declare @cnt int
+declare @amount int
+declare @sum int
+declare @date date
+set @date = @InstallStartDate
+select @cnt = num_installments from payment where ID = @paymentID
+select @sum = total_amount from payment where ID = @paymentID
+set @amount = @sum/@cnt
+while(@cnt > 0)
+begin
+	insert into installment (pid, date, status, amount)
+	values (@paymentID, @date, 0, @amount)
+	set @date = DATEADD(month, 6, @date)
+	set @cnt -= 1
+end
+
+go
+create proc AddCourse
+@courseCode varchar(10), 
+@creditHrs int, 
+@fees decimal
+as
+insert into course (code, credit_hours, fees) 
+values (@courseCode, @creditHrs, @fees)
+
+go
+create proc linkCourseStudent
+@courseID int,
+@studentID int
+as
+insert into take_course (sid, cid) 
+values (@studentID, @courseID)
+
+go
+create proc addStudentCourseGrade
+@courseID int, 
+@studentID int, 
+@grade decimal
+as
+update take_course
+set grade = @grade
+where sid = @studentID and cid = @courseID
