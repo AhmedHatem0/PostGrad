136350b4cd9f4c404e07391a6f1fe822923e0159
Kareem eid <kareem.m.eid@gmail.com>
2021-12-15 19:19:05 +0200

    Update Insertion.sql
    added 15 theses

--- Insertion.sql
+++ Insertion.sql
@@ -45,4 +45,20 @@ update NonGUCianStudent
 set type = @type where @ID = ID 
 end
 
-
+insert into thesis (field, type, title, start_date, end_date) 
+values 
+('Computer Science', 'MSC', 'User interface modernization', '3/20/2015', '11/20/2015'),
+('Computer Science', 'MSC', 'Development of an automated workplace', '5/30/2019', '1/2/2020'),
+('Computer Science', 'MSC', 'Online store development and creation', '7/25/2021', '2/25/2022'),
+('Medicine', 'PHD', 'The influence of IT in the field of biomedicine', '8/20/2021', '4/25/2024'),
+('Art', 'MSC', 'Gustav Klimt’s Painting Techniques', '10/20/2021', '6/25/2022'),
+('Art', 'MSC', 'Jazz Music of the 21st century', '10/20/2017', '10/25/2018'),
+('Art', 'PHD', 'Censorship of Art in Nazi Germany', '10/20/2020', '6/25/2024'),
+('Business', 'PHD', 'How can creative marketing increase your sales?', '10/10/2019', '6/10/2023'),
+('Law', 'PHD', 'Analysis of Welfare Politics in Japan', '9/9/2020', '6/10/2025'),
+('Business', 'MSC', 'Important aspects of workforce regulations', '1/24/2019', '4/25/2020'),
+('Engineering', 'PHD', 'Self-driving automobiles', '5/20/2019', '9/9/2024'),
+('Engineering', 'MSC', 'What is the future of the Internet?', '8/20/2019', '9/9/2020'),
+('Computer Science', 'MSC', 'Health problems caused by service time', '8/8/2017', '10/10/2018'),
+('Art', 'PHD', 'Ancient Greek Sculpture and Its Peculiarities', '11/11/2020', '11/11/2024'),
+('Business', 'MSC', 'How to motivate employees?', '6/6/2019', '5/5/2020')
