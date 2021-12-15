d0fda62ba8bbbcc3a24882d404072a5b388f1bcb
Aboda81 <abdo.hussein812@gmail.com>
2021-12-15 14:07:27 +0200

    Insertion

---
+++ Insertion.sql
@@ -0,0 +1,54 @@
+﻿Create proc AddExaminer
+@name varcHAR(20),
+@fieldOfWork VARCHAR(20) ,
+@isNational bit,
+@email varchar(50),
+@password varchar(20)
+As
+insert into PostGradUser Values (@email, @password)
+insert into Examiner Values (IDENT_CURRENT('PostGradUser'),@name, @fieldOfWork,@isNational)
+
+go
+
+create proc AddAdmin @emil varchar(50) ,@password varchar(20)
+as 
+insert into PostGradUser Values (@email, @password)
+insert into Admin Values (IDENT_CURRENT('PostGradUser'))
+
+go 
+create proc AddGPA  @ID int , @GPA decimal
+As 
+if Exists(select * from GUCianStudent where @ID  =ID)
+begin
+update GUCianStudent 
+set GPA = @Gpa where @ID = ID 
+end
+Else
+if Exists(select * from NonGUCianStudent where @ID  =ID)
+begin
+update NonGUCianStudent 
+set GPA = @Gpa where @ID = ID 
+end
+
+go
+create proc AddType @ID int , @type varchar(10)
+As 
+if Exists(select * from GUCianStudent where @ID  =ID)
+begin
+update GUCianStudent 
+set type = @type where @ID = ID 
+end
+Else
+if Exists(select * from NonGUCianStudent where @ID  =ID)
+begin
+update NonGUCianStudent 
+set type = @type where @ID = ID 
+end
+
+
+
+
+
+
+exec StudentRegister 'abdelrahman', 'Hussein', '123456789','Engineering',
+'1', 'abdel-rahman.mahmoud', 'Suez'
